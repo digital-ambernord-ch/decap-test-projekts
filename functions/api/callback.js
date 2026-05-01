d5f5e91f5d713f3069c6cda8873e1fe30a71da59
digital-ambernord-ch <erik.matisons@gmail.com>
2026-05-01 11:15:30 +0200

    Update callback.js

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -5,22 +5,30 @@ export async function onRequest(context) {
 
   const response = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-    body: JSON.stringify({ client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code })
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      code: code
+    })
   });
 
   const data = await response.json();
   const token = data.access_token;
-  const content = JSON.stringify({ token, provider: 'github' });
 
-  return new Response(
-    `<!DOCTYPE html><html><body><script>
-    (function() {
-      var msg = 'authorization:github:success:' + ${JSON.stringify(content)};
-      window.opener.postMessage(msg, location.origin.replace('github-oauth-proxy.eriksmatisonsvd.workers.dev', 'ambernord-admin.pages.dev'));
-      window.close();
-    })();
-    <\/script></body></html>`,
-    { headers: { 'Content-Type': 'text/html' } }
-  );
+  const html = '<!DOCTYPE html><html><body><script>' +
+    '(function(){' +
+    'var d={"token":"' + token + '","provider":"github"};' +
+    'var m="authorization:github:success:"+JSON.stringify(d);' +
+    'window.opener.postMessage(m,"https://ambernord-admin.pages.dev");' +
+    'window.close();' +
+    '})();' +
+    '<\/script></body></html>';
+
+  return new Response(html, {
+    headers: { 'Content-Type': 'text/html' }
+  });
 }
